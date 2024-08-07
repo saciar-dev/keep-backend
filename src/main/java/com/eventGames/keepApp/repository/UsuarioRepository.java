ea718d3af42ebad57e3c567d01da6a0725c3d20f
saciar-dev <aciarenator@gmail.com>
2024-08-07 11:44:34 -0300

    se agrega target

--- src/main/java/com/eventGames/keepApp/repository/UsuarioRepository.java
+++ src/main/java/com/eventGames/keepApp/repository/UsuarioRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface UsuarioRepository  extends JpaRepository<Usuario, Long> {
 
-    @Query("select c from Usuario c where c.nombre= ?1 or c.telefono= ?2")
+    @Query("select c from Usuario c where c.nombre= ?1 and c.telefono= ?2")
     List<Usuario> getUsuarioByNameAndPhone(String nombre, String telefono);
 }
